55a92d3c8a06b2b8709a592246c2c500bed7c31b
Madan Raj M A <madanrajma.22aid@kongu.edu>
2025-04-14 12:42:41 +0530

    Time: 3 ms (22.93%) | Memory: 46.2 MB (32.21%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int left = 0;
+        int tvar=0;
+        for (int i =0;i<nums.length;i++)
+        {
+            if(nums[i]>0 || nums[i]<0)
+            {
+                tvar = nums[left];
+                nums[left] = nums[i];
+                nums[i]=tvar;
+                left++;
+            }
+        }
+    }
+}
