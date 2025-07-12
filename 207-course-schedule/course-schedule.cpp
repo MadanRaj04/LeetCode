1d2bed39d88c2b3bc0aec948e8c9f506739a100d
Madan Raj M A <madanrajma.22aid@kongu.edu>
2025-07-12 21:51:53 +0530

    Time: 6 ms (54.53%) | Memory: 20.4 MB (17.32%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+
+    unordered_map<int,vector<int>> buildadj(vector<vector<int>>& prerequisites)
+    {
+        unordered_map<int,vector<int>> mp;
+        for(int i=0;i<prerequisites.size();i++)
+        {
+            mp[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        }
+        return mp;
+    }
+
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int,vector<int>> mp = buildadj(prerequisites);
+        vector<int> indegree(numCourses,0);
+        for(int i=0;i<mp.size();i++)
+        {
+            for(auto it:mp[i])
+            {
+                indegree[it]++;
+            }
+        }
+
+        queue<int> zero;
+        queue<int> res;
+        for(int i=0;i<numCourses;i++)
+        {
+            if(indegree[i]==0)
+            {
+                zero.push(i);
+            }
+        }
+        while(!zero.empty())
+        {
+            int cur = zero.front();
+            zero.pop();
+            res.push(cur);
+            for(auto it:mp[cur])
+            {
+                indegree[it]--;
+
+                if(indegree[it]==0) zero.push(it);
+            }
+        }
+        if(res.size()!= numCourses)
+        {
+            return false;
+        }
+        return true;
+
+    }
+};
