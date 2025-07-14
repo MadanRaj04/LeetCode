3abf40a48fb6992549d37c31a6fb83110395a910
Madan Raj M A <madanrajma.22aid@kongu.edu>
2025-07-14 14:26:30 +0530

    Time: 10 ms (7.23%) | Memory: 18.1 MB (6.44%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
+    const vector<vector<int>> directions = {{0,1},{0,-1},{1,0},{-1,0}};
     int orangesRotting(vector<vector<int>>& grid) {
-        const vector<vector<int>> directions = {{1,0},{-1,0},{0,1},{0,-1}};
+        queue<pair<int,int>> q;
         int ones=0;
-        queue<pair<int,int>> qp;
         for(int i=0;i<grid.size();i++)
         {
             for(int j=0;j<grid[0].size();j++)
             {
                 if(grid[i][j]==2)
                 {
-                    qp.push({i,j});
+                    q.push({i,j});
                 }
                 else if(grid[i][j]==1)
                 {
@@ -18,37 +18,31 @@ public:
                 }
             }
         }
-        if(qp.empty() && ones==0)
-        {
-
-            return 0;
-        }
-
-
+        if(ones==0) return 0;
         int count=0;
-        while(!qp.empty())
+        while(!q.empty())
         {
-            int size = qp.size();
+            int size = q.size();
             for(int i=0;i<size;i++)
             {
-                auto [r,c] = qp.front();qp.pop();
-
-                for(auto& it:directions)
+                auto [r,c] = q.front();q.pop();
+                
+                for(auto it:directions)
                 {
                     int nx = r+it[0];
                     int ny = c+it[1];
-                    if(nx>=0 && ny >= 0 && nx<grid.size() && ny<grid[0].size() &&grid[nx][ny]==1)
+                    if(nx>=0 && ny >=0 && nx<grid.size() && ny <grid[0].size() && grid[nx][ny]==1)
                     {
                         grid[nx][ny]=2;
-                        qp.push({nx,ny});
                         ones--;
+                        q.push({nx,ny});
                     }
+                    
                 }
             }
-            count+=1;
+            count++;
         }
-        if(ones!=0) return -1;
-
-        return count == 0 ? 0 : count - 1;
+//        cout<<ones<<endl;
+        return (ones==0)?(count-1):-1;
     }
 };
